691ded1e4940902be39255ad3f4323cdc1c14ed8
moneshkannan <moneshkannan02@gmail.com>
2024-03-05 15:15:09 +0530

    3 Allow the add method to handle new lines between numbers (instead of commas

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 def add(numbers)
     return 0 if numbers.empty?
-    numbers_list = numbers.split(",")
+    numbers_list = numbers.gsub("\n", ",").split(",")
     sum = 0
     numbers_list.each { |num| sum += num.to_i }
     sum
@@ -13,3 +13,6 @@ puts add("1,5")
 # 2 Allow the add method to handle an any amount of numbers.
 puts add("1,2,3,4,5")
 puts add("10,20,30,40,50")
+# 3 Allow the add method to handle new lines between numbers (instead of commas
+puts add("1\n2,3")    # Output: 6
+puts add("10\n20,30") # Output: 60
